b1e823d517709c7a1b659db9e0f51fa947277952
Techcorp07 <poso.draxy@gmail.com>
2025-10-25 22:38:23 +0200

    prod settings fix

--- config/settings.py
+++ config/settings.py
@@ -94,12 +94,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = config('STATIC_ROOT', default='/var/www/mri-training-platform/static')
-#STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = config('MEDIA_ROOT', default='/var/www/mri-training-platform/media')
-#MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
